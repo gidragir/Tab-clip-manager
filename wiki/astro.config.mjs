431ac7a0c884e057f8ebcb0a3804327c8bfd339f
gidragir <ilyabalter1@gmail.com>
2024-10-14 01:44:05 +0500

    wiki/astro.config:

--- wiki/astro.config.mjs
+++ wiki/astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: "https://gidragir.github.io",
-	base: "/Tab-clip-manager/wiki",
+	base: "/wiki",
 	integrations: [
 		starlight({
 			title: 'My Docs',
