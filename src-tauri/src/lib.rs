e8e01ffba7f07dce2202426b2103af62c638832a
gidragir <ilyabalter1@gmail.com>
2024-10-20 11:27:33 +0500

    add settings window

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -9,8 +9,9 @@ pub fn run() {
         .setup(|app| {
             let open = MenuItem::with_id(app, "open", "Open", true, None::<&str>)?;
             let hide = MenuItem::with_id(app, "hide", "Hide", true, None::<&str>)?;
+            let settings = MenuItem::with_id(app, "settings", "Settings", true, None::<&str>)?;
             let quit = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
-            let menu = Menu::with_items(app, &[&open, &hide, &quit])?;
+            let menu = Menu::with_items(app, &[&open, &hide, &quit, &settings])?;
 
             let _tray = TrayIconBuilder::new()
                 .icon(app.default_window_icon().unwrap().clone())
@@ -21,15 +22,20 @@ pub fn run() {
         })
         .on_menu_event(|app, event| match event.id.as_ref() {
             "open" => {
-                let window = app.get_webview_window("main").unwrap();
+                let window = app.get_webview_window("Tab_Clip_Manager_main").unwrap();
                 let _ = window.show();
                 let _ = window.set_focus();
             }
             "hide" => {
-                app.get_webview_window("main")
+                app.get_webview_window("Tab_Clip_Manager_main")
                 .unwrap()
                 .hide()
                 .unwrap();
+            }
+            "settings" => {
+                let window = app.get_webview_window("Tab_Clip_Manager_settings").unwrap();
+                let _ = window.show();
+                let _ = window.set_focus();
             }
             "quit" => {
                 app.exit(0);
