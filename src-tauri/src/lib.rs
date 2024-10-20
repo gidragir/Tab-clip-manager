cb7fd71340d0c59f54e81ab049906b9934c3e63d
gidragir <ilyabalter1@gmail.com>
2024-10-20 13:02:41 +0500

    src-tauri/src: extract tray menu code to a new module

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,49 +1,18 @@
 use tauri::{
     menu::{Menu, MenuItem},
-    tray::TrayIconBuilder, Manager,
+    tray::TrayIconBuilder,
+    Manager,
 };
+mod tray_menu;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .setup(|app| {
-            let open = MenuItem::with_id(app, "open", "Open", true, None::<&str>)?;
-            let hide = MenuItem::with_id(app, "hide", "Hide", true, None::<&str>)?;
-            let settings = MenuItem::with_id(app, "settings", "Settings", true, None::<&str>)?;
-            let quit = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
-            let menu = Menu::with_items(app, &[&open, &hide, &quit, &settings])?;
-
-            let _tray = TrayIconBuilder::new()
-                .icon(app.default_window_icon().unwrap().clone())
-                .menu(&menu)
-                .menu_on_left_click(true)
-                .build(app)?;
+            tray_menu::setup_tray_menu(app)?;
             Ok(())
         })
-        .on_menu_event(|app, event| match event.id.as_ref() {
-            "open" => {
-                let window = app.get_webview_window("Tab_Clip_Manager_main").unwrap();
-                let _ = window.show();
-                let _ = window.set_focus();
-            }
-            "hide" => {
-                app.get_webview_window("Tab_Clip_Manager_main")
-                .unwrap()
-                .hide()
-                .unwrap();
-            }
-            "settings" => {
-                let window = app.get_webview_window("Tab_Clip_Manager_settings").unwrap();
-                let _ = window.show();
-                let _ = window.set_focus();
-            }
-            "quit" => {
-                app.exit(0);
-            }
-            _ => {
-                println!("menu item {:?} not handled", event.id);
-            }
-        })
+        .on_menu_event(|app, event| tray_menu::on_menu_event(app, event))        
         .plugin(tauri_plugin_shell::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
---
+++ src-tauri/src/tray_menu.rs
@@ -0,0 +1,51 @@
+use tauri::{
+    menu::{Menu, MenuEvent, MenuItem}, 
+    tray::TrayIconBuilder, 
+    App, 
+    AppHandle, 
+    Manager,
+    Error
+};
+
+pub fn setup_tray_menu(app: &mut App) -> Result<(), Error> {
+    let open = MenuItem::with_id(app, "open", "Open", true, None::<&str>)?;
+    let hide = MenuItem::with_id(app, "hide", "Hide", true, None::<&str>)?;
+    let settings = MenuItem::with_id(app, "settings", "Settings", true, None::<&str>)?;
+    let quit = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
+
+    let menu = Menu::with_items(app, &[&open, &hide, &settings, &quit])?;
+
+    let _tray = TrayIconBuilder::new()
+        .icon(app.default_window_icon().unwrap().clone())
+        .menu(&menu)
+        .menu_on_left_click(true)
+        .build(app)?;
+    Ok(())
+}
+
+pub fn on_menu_event(app: &AppHandle, event: MenuEvent) {
+    match event.id.as_ref() {
+        "open" => {
+            let window = app.get_webview_window("Tab_Clip_Manager_main").unwrap();
+            let _ = window.show();
+            let _ = window.set_focus();
+        }
+        "hide" => {
+            app.get_webview_window("Tab_Clip_Manager_main")
+                .unwrap()
+                .hide()
+                .unwrap();
+        }
+        "settings" => {
+            let window = app.get_webview_window("Tab_Clip_Manager_settings").unwrap();
+            let _ = window.show();
+            let _ = window.set_focus();
+        }
+        "quit" => {
+            app.exit(0);
+        }
+        _ => {
+            println!("menu item {:?} not handled", event.id);
+        }
+    }
+}
