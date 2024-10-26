669fb87fbba77884235c1ea83b679998fac26ec8
gidragir <ilyabalter1@gmail.com>
2024-10-27 01:23:51 +0500

    add sidebar menu to settings

--- src/windows/settings/App.tsx
+++ src/windows/settings/App.tsx
@@ -1,13 +1,77 @@
 import "@/style/App.scss"
 import { ModeToggle } from "@/components/theme/ModeToggle"
+import { SidebarProvider } from "@/components/ui/sidebar"
+import { Home, Keyboard } from "lucide-react"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem
+} from "@/components/ui/sidebar"
 
-function App() {
+// Menu items.
+const items = [
+  {
+    title: "Home",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "Hotkeys",
+    url: "#",
+    icon: Keyboard,
+  },
+]
 
+
+export default function App() {
   return (
-    <main>
-      <ModeToggle />
-    </main>
+    <SidebarProvider className="h-20">
+      <Sidebar collapsible="none">
+        <SidebarContent>
+          <SidebarGroup>
+            <SidebarGroupLabel>Tab clip manager</SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+              <ModeToggle />
+                {items.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton asChild>
+                      <a href={item.url}>
+                        <item.icon />
+                        <span>{item.title}</span>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        </SidebarContent>
+      </Sidebar>
+    </SidebarProvider>
   )
 }
-
-export default App
+{/* <SidebarContent>
+<SidebarGroup>
+  <SidebarGroupLabel>Tab clip manager</SidebarGroupLabel>
+  <SidebarGroupContent>
+    <SidebarMenu>
+      {items.map((item) => (
+        <SidebarMenuItem key={item.title}>
+          <SidebarMenuButton asChild>
+            <a href={item.url}>
+              <item.icon />
+              <span>{item.title}</span>
+            </a>
+          </SidebarMenuButton>
+        </SidebarMenuItem>
+      ))}
+    </SidebarMenu>
+  </SidebarGroupContent>
+</SidebarGroup>
+</SidebarContent> */}
