eaa0e415f0d5b96a099189caf050e8dd7faa6f81
gidragir <ilyabalter1@gmail.com>
2024-11-17 22:38:34 +0500

    src-tauri: update dep

--- src-tauri/src/shortcuts.rs
+++ src-tauri/src/shortcuts.rs
@@ -1,4 +1,4 @@
-use tauri::{App, Error};
+use tauri::{App, AppHandle, Manager, Error};
 use tauri_plugin_global_shortcut::{
     Code, GlobalShortcutExt, Modifiers, Shortcut, ShortcutEvent, ShortcutState,
 };
@@ -8,7 +8,7 @@ pub fn add_shortcuts(app: &mut App) -> Result<(), Error> {
   app.handle().plugin(
       tauri_plugin_global_shortcut::Builder::new()
           .with_handler(move |_app, shortcut, event| {
-              main_shortcut_handler(shortcut, event);
+              main_shortcut_handler(_app, shortcut, event);
           })
           .build(),
   )?;
@@ -21,17 +21,20 @@ fn main_shortcut() -> Shortcut {
   Shortcut::new(Some(Modifiers::ALT | Modifiers::SUPER), Code::KeyV)
 }
 
-fn add_shortcut(app: &mut App, shortcut: Shortcut) {
-  let _ = app.global_shortcut().register(shortcut);
-}
-
-fn main_shortcut_handler(shortcut: &Shortcut, event: ShortcutEvent) {
+fn main_shortcut_handler(app: &AppHandle, shortcut: &Shortcut, event: ShortcutEvent) {
   if shortcut == &main_shortcut() {
       match event.state() {
           ShortcutState::Pressed => {}
           ShortcutState::Released => {
-              println!("Ctrl-N Released!");
+            let window = app.get_webview_window("Tab_Clip_Manager_main").unwrap();
+            let _ = &window.show();
+            let _ = &window.set_focus();
           }
       }
   }
+}
+
+
+fn add_shortcut(app: &mut App, shortcut: Shortcut) {
+  let _ = app.global_shortcut().register(shortcut);
 }
