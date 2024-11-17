7138eb16b4b1643572396befc7a06c681363c28f
gidragir <ilyabalter1@gmail.com>
2024-11-17 23:21:47 +0500

    add shortcuts

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,10 +1,11 @@
 use tauri;
-mod tray_menu;
 mod shortcuts;
+mod tray_menu;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_store::Builder::new().build())
         .setup(|app| {
             tray_menu::setup_tray_menu(app)?;
--- src-tauri/src/shortcuts.rs
+++ src-tauri/src/shortcuts.rs
@@ -1,40 +1,108 @@
-use tauri::{App, AppHandle, Manager, Error};
+use tauri::{App, AppHandle, Error, Manager};
 use tauri_plugin_global_shortcut::{
     Code, GlobalShortcutExt, Modifiers, Shortcut, ShortcutEvent, ShortcutState,
 };
+use tauri_plugin_clipboard_manager::ClipboardExt;
 
 #[cfg(desktop)]
 pub fn add_shortcuts(app: &mut App) -> Result<(), Error> {
-  app.handle().plugin(
-      tauri_plugin_global_shortcut::Builder::new()
-          .with_handler(move |_app, shortcut, event| {
-              main_shortcut_handler(_app, shortcut, event);
-          })
-          .build(),
-  )?;
+    app.handle().plugin(
+        tauri_plugin_global_shortcut::Builder::new()
+            .with_handler(move |_app, shortcut, event| {
+                main_shortcut_handler(_app, shortcut, event);
+                copy_shortcut_handler(_app, shortcut, event);
+                cut_shortcut_handler(_app, shortcut, event);
+                paste_shortcut_handler(_app, shortcut, event);
+            })
+            .build(),
+    )?;
 
-  add_shortcut(app, main_shortcut());
-  Ok(())
+    let shortcuts = [
+      main_shortcut(),
+      copy_shortcut(),
+      cut_shortcut(),
+      paste_shortcut(),
+    ];
+
+    for shortcut in shortcuts {
+        add_shortcut(app, shortcut);
+    }
+    Ok(())
 }
 
+// region: Shortcuts
 fn main_shortcut() -> Shortcut {
-  Shortcut::new(Some(Modifiers::ALT | Modifiers::SUPER), Code::KeyV)
+    Shortcut::new(Some(Modifiers::ALT | Modifiers::SUPER), Code::KeyV)
 }
 
+fn copy_shortcut() -> Shortcut {
+  Shortcut::new(Some(Modifiers::CONTROL), Code::KeyC)
+}
+
+fn cut_shortcut() -> Shortcut {
+  Shortcut::new(Some(Modifiers::CONTROL), Code::KeyX)
+}
+
+fn paste_shortcut() -> Shortcut {
+  Shortcut::new(Some(Modifiers::CONTROL), Code::KeyV)
+}
+// endregion
+
+// region: Shortcuts handler
 fn main_shortcut_handler(app: &AppHandle, shortcut: &Shortcut, event: ShortcutEvent) {
-  if shortcut == &main_shortcut() {
-      match event.state() {
-          ShortcutState::Pressed => {}
-          ShortcutState::Released => {
+    if shortcut != &main_shortcut() {
+        return;
+    }
+    match event.state() {
+        ShortcutState::Pressed => {}
+        ShortcutState::Released => {
             let window = app.get_webview_window("Tab_Clip_Manager_main").unwrap();
             let _ = &window.show();
             let _ = &window.set_focus();
-          }
-      }
-  }
+        }
+    }
 }
 
+fn copy_shortcut_handler(app: &AppHandle, shortcut: &Shortcut, event: ShortcutEvent) {
+    if shortcut != &copy_shortcut() {
+        return;
+    }
+    match event.state() {
+        ShortcutState::Pressed => {}
+        ShortcutState::Released => {
+          let content = app.clipboard().read_text();
+          println!("{:?}", content.unwrap());
+        }
+    }
+}
+
+fn cut_shortcut_handler(app: &AppHandle, shortcut: &Shortcut, event: ShortcutEvent) {
+    if shortcut != &cut_shortcut() {
+        return;
+    }
+    match event.state() {
+        ShortcutState::Pressed => {}
+        ShortcutState::Released => {
+          let content = app.clipboard().read_text();
+          println!("{:?}", content.unwrap());
+        }
+    }
+}
+
+fn paste_shortcut_handler(app: &AppHandle, shortcut: &Shortcut, event: ShortcutEvent) {
+    if shortcut != &paste_shortcut() {
+        return;
+    }
+    match event.state() {
+        ShortcutState::Pressed => {}
+        ShortcutState::Released => {
+          let content = app.clipboard().read_text();
+          println!("{:?}", content.unwrap());
+        }
+    }
+}
+// endregion
 
 fn add_shortcut(app: &mut App, shortcut: Shortcut) {
-  let _ = app.global_shortcut().register(shortcut);
+    let _ = app.global_shortcut().register(shortcut);
 }
