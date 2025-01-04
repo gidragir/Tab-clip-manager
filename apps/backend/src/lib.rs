f9449cb08d51c6dc82f3249d283ad0719ee1a6e8
gidragir <ilyabalter1@gmail.com>
2025-01-05 00:59:49 +0500

    add scripts

--- apps/backend/src/lib.rs
+++ apps/backend/src/lib.rs
@@ -5,6 +5,7 @@ mod tray_menu;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_sql::Builder::default().build())
         .plugin(tauri_plugin_clipboard_manager::init())
         .setup(|app| {
             tray_menu::setup_tray_menu(app)?;
