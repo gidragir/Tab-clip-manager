6f2fc74039f65f2a9736d280a44382e3390c4edd
gidragir <ilyabalter1@gmail.com>
2025-01-08 05:20:45 +0500

    change prisma

--- apps/backend/tauri/src/lib.rs
+++ apps/backend/tauri/src/lib.rs
@@ -2,11 +2,13 @@ use tauri::{self, Emitter};
 mod shortcuts;
 mod tray_menu;
 
+#[allow(warnings)]
+mod prisma;
 
-use prisma_cli;
+use prisma::PrismaClient;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
-pub fn run() {
+pub async fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_clipboard_manager::init())
         .setup(|app| {
--- apps/backend/tauri/src/main.rs
+++ apps/backend/tauri/src/main.rs
@@ -1,6 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-fn main() {
-    tab_clip_manager_lib::run()
+#[tokio::main]
+async fn main() {
+    tab_clip_manager_lib::run().await;
 }
